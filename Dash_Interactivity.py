57a2e156d7e177de6ef31b0610ba83eb44e461d2
tinouchemassinissa <127163781+tinouchemassinissa@users.noreply.github.com>
2023-04-16 23:27:07 -0700

    Add files via upload

---
+++ Dash_Interactivity.py
@@ -0,0 +1,3 @@
+wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv"
+wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/labs/module_3/spacex_dash_app.py"
+python3.8 spacex_dash_app.py
